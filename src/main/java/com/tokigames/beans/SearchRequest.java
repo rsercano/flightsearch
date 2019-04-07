886de693809779de51250636fdfc5cb8caea4658
sercan <ozdemirsercan27@gmail.com>
2019-04-07 09:23:48 +0300

    search tests implemented

--- src/main/java/com/tokigames/beans/SearchRequest.java
+++ src/main/java/com/tokigames/beans/SearchRequest.java
@@ -1,20 +1,29 @@
 package com.tokigames.beans;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.tokigames.model.Flight;
+import java.util.Map;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.domain.Sort.Direction;
 
+/**
+ * Request of search method, uses AND for all given parameters for {@link SearchRequest#query}.
+ */
 @Getter
 @Setter
 @ToString
+@Builder
+@JsonDeserialize(builder = SearchRequest.SearchRequestBuilder.class)
 public class SearchRequest {
 
   private Flight query;
 
-  private int pageSize = 10;
-  private int page = 0;
+  private int pageSize;
+  private int page;
 
-  private Direction sortBy;
+  private Map<String, Direction> sortBy;
+
 }
--- src/test/java/com/tokigames/service/FlightSearchHandlerTests.java
+++ src/test/java/com/tokigames/service/FlightSearchHandlerTests.java
@@ -6,6 +6,8 @@ import static org.mockito.Mockito.when;
 import com.tokigames.SpringTestConfiguration;
 import com.tokigames.beans.BusinessFlight;
 import com.tokigames.beans.CheapFlight;
+import com.tokigames.beans.SearchRequest;
+import com.tokigames.beans.SearchResult;
 import com.tokigames.exception.CommunicationException;
 import com.tokigames.model.Flight;
 import com.tokigames.util.ModelConverterUtil;
@@ -14,12 +16,13 @@ import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ContextConfiguration;
@@ -53,7 +56,7 @@ public class FlightSearchHandlerTests {
     CheapFlight cheapFlight2 = new CheapFlight("2", "FRA", "AYT", Instant.now().toEpochMilli(), Instant.now().plus(3, ChronoUnit.HOURS).toEpochMilli());
 
     BusinessFlight businessFlight = new BusinessFlight("3", "AYT -> GZT", ZonedDateTime.parse("2019-04-06T10:20:33Z").toLocalDateTime(),
-        ZonedDateTime.parse("2019-04-06T10:20:33Z").toLocalDateTime().plus(1, ChronoUnit.HOURS));
+        ZonedDateTime.parse("2019-04-06T10:20:33Z").toLocalDateTime().plusHours(1));
 
     when(httpRequestUtil.proceedGetRequest(SpringTestConfiguration.CHEAP_FLIGHTS_URL, new ParameterizedTypeReference<List<CheapFlight>>() {
     })).thenReturn(List.of(cheapFlight, cheapFlight2));
@@ -107,4 +110,130 @@ public class FlightSearchHandlerTests {
     cut.cacheFlights();
   }
 
+  /**
+   * Simple search without any query & sort, should come with insertion order.
+   */
+  @Test
+  public void searchTest_1() {
+    // arrange
+    List<Flight> flights = createFlights();
+
+    // execute
+    SearchResult firstPage = cut.search(SearchRequest.builder().page(0).pageSize(1).build());
+    SearchResult secondPage = cut.search(SearchRequest.builder().page(1).pageSize(1).build());
+
+    // assert
+    assertThat(firstPage).isNotNull();
+    assertThat(firstPage.getResult().size()).isEqualTo(1);
+    assertThat(firstPage.getResult().get(0)).isEqualTo(flights.get(0));
+    assertThat(firstPage.getTotal()).isEqualTo(6);
+
+    assertThat(secondPage).isNotNull();
+    assertThat(secondPage.getResult().size()).isEqualTo(1);
+    assertThat(secondPage.getResult().get(0)).isEqualTo(flights.get(1));
+    assertThat(secondPage.getTotal()).isEqualTo(6);
+  }
+
+  /**
+   * Search with one field query & with single sort.
+   */
+  @Test
+  public void searchTest_2() {
+    // arrange
+    List<Flight> flights = createFlights();
+
+    // execute
+    SearchResult result = cut.search(SearchRequest.builder()
+        .page(0)
+        .pageSize(1)
+        .sortBy(Map.of("arrivalTime", Direction.ASC))
+        .query(Flight.builder().arrival("AYT").build())
+        .build());
+
+    // assert
+    assertThat(result).isNotNull();
+    assertThat(result.getResult().size()).isEqualTo(1);
+    assertThat(result.getResult().get(0)).isEqualTo(flights.get(4));
+    assertThat(result.getTotal()).isEqualTo(4);
+  }
+
+  /**
+   * Search with multiple queries & single sort.
+   */
+  @Test
+  public void searchTest_3() {
+    // arrange
+    List<Flight> flights = createFlights();
+
+    // execute
+    SearchResult result = cut.search(SearchRequest.builder()
+        .page(0)
+        .pageSize(5)
+        .sortBy(Map.of("arrivalTime", Direction.ASC))
+        .query(Flight.builder().arrival("AYT").departure("GZP").build())
+        .build());
+
+    // assert
+    assertThat(result).isNotNull();
+    assertThat(result.getResult().size()).isEqualTo(3);
+    assertThat(result.getResult().get(0)).isEqualTo(flights.get(4));
+    assertThat(result.getResult().get(1)).isEqualTo(flights.get(5));
+    assertThat(result.getResult().get(2)).isEqualTo(flights.get(3));
+    assertThat(result.getTotal()).isEqualTo(3);
+  }
+
+  /**
+   * Search with multiple queries & multiple sort.
+   */
+  @Test
+  public void searchTest_4() {
+    // arrange
+    List<Flight> flights = createFlights();
+
+    // execute
+    SearchResult result = cut.search(SearchRequest.builder()
+        .page(0)
+        .pageSize(5)
+        .sortBy(Map.of("arrivalTime", Direction.ASC, "flightId", Direction.DESC))
+        .query(Flight.builder().arrival("AYT").departure("GZP").build())
+        .build());
+
+    // assert
+    assertThat(result).isNotNull();
+    assertThat(result.getResult().size()).isEqualTo(3);
+    assertThat(result.getResult().get(0)).isEqualTo(flights.get(4));
+    assertThat(result.getResult().get(1)).isEqualTo(flights.get(5));
+    assertThat(result.getResult().get(2)).isEqualTo(flights.get(3));
+    assertThat(result.getTotal()).isEqualTo(3);
+  }
+
+  private List<Flight> createFlights() {
+    List<Flight> result = new ArrayList<>();
+
+    Flight flight1 = Flight.builder().arrival("AYT").arrivalTime(ZonedDateTime.parse("2019-04-06T21:11:10Z").toLocalDateTime()).flightId("1").departure("GZT")
+        .departureTime(ZonedDateTime.parse("2019-04-06T22:11:10Z").toLocalDateTime().plusHours(1)).build();
+    Flight flight2 = Flight.builder().arrival("SGP").arrivalTime(ZonedDateTime.parse("2019-04-06T10:01:02Z").toLocalDateTime()).flightId("2").departure("AYT")
+        .departureTime(ZonedDateTime.parse("2019-04-06T10:01:02Z").toLocalDateTime().plusHours(10)).build();
+    Flight flight3 = Flight.builder().arrival("GZT").arrivalTime(ZonedDateTime.parse("2019-04-07T05:05:00Z").toLocalDateTime()).flightId("3").departure("SGP")
+        .departureTime(ZonedDateTime.parse("2019-04-07T05:05:00Z").toLocalDateTime().plusHours(9)).build();
+    Flight flight4 = Flight.builder().arrival("AYT").arrivalTime(ZonedDateTime.parse("2019-04-07T08:10:00Z").toLocalDateTime()).flightId("4").departure("GZP")
+        .departureTime(ZonedDateTime.parse("2019-04-07T08:10:00Z").toLocalDateTime().plusHours(1)).build();
+    Flight flight5 = Flight.builder().arrival("AYT").arrivalTime(ZonedDateTime.parse("2019-04-06T10:10:30Z").toLocalDateTime()).flightId("5").departure("GZP")
+        .departureTime(ZonedDateTime.parse("2019-04-06T10:10:30Z").toLocalDateTime().plusHours(1)).build();
+    Flight flight6 = Flight.builder().arrival("AYT").arrivalTime(ZonedDateTime.parse("2019-04-06T11:10:30Z").toLocalDateTime()).flightId("6").departure("GZP")
+        .departureTime(ZonedDateTime.parse("2019-04-06T11:10:30Z").toLocalDateTime().plusHours(1)).build();
+
+    mongoTemplate.save(flight1);
+    mongoTemplate.save(flight2);
+    mongoTemplate.save(flight3);
+    mongoTemplate.save(flight4);
+    mongoTemplate.save(flight5);
+    mongoTemplate.save(flight6);
+
+    result.add(flight1);
+    result.add(flight2);
+    result.add(flight3);
+
+    return result;
+  }
 }
